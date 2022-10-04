b524df36bc101ee8f588c1f46cf1307a8e47a67b
Agostina <lozagostina@gmail.com>
2022-10-04 17:36:42 -0300

    agrego spinner como componente condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button} from "react-bootstrap";
 import titulosimpsons from "./assets/titulo.png";
 import Frase from "./Frase";
 import './style.css'
 import {useEffect, useState} from 'react';
+import Spinner from "./Spinner";
 
 function App() {
 
 const[personaje, setPersonaje]=useState({});
+const [mostrarSpinner, setMostrarSpinner]=useState(true);
 
   useEffect(()=>{
 consultarAPI();
@@ -16,17 +18,26 @@ consultarAPI();
 const consultarAPI= async()=>{
   
   try{
+    //mostrar el spinner
+    setMostrarSpinner(true)
       const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
       console.log(respuesta)
       const dato = await respuesta.json();
       console.log(dato [0]);
       setPersonaje(dato[0]);
+      //mostrar frase
+      setMostrarSpinner(false)
 
     }catch(error){
 console.log(error);
 //mostrar un mensaje al usuario  
     }
 }
+
+//condicion logica if, operador ternario
+//(condicion logica)?(logica a ejecutar si cumplo la condicion):(logica a ejecutar si no se cumple la condicion)
+  
+const mostrarComponente = (mostrarSpinner=== true ? (  <Spinner></Spinner>):(<Frase personaje={personaje}></Frase>))
 
   return (
     <>
@@ -35,9 +46,8 @@ console.log(error);
       <br></br>
       <div className="text-center mb-5">
       <Button variant="warning" onClick={consultarAPI} >Obtener frase</Button>{' '}
-
-      </div>
-    <Frase personaje={personaje}></Frase>
+       </div>
+{mostrarComponente}
     </Container>
     </>
   );
---
+++ src/Spinner.jsx
@@ -0,0 +1,12 @@
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <section className='d-flex justify-content-center'>
+            <div className="spinner"></div>
+
+        </section>
+    );
+};
+
+export default Spinner;
